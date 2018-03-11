e722696ee056fdaf7fa1380fffec0d7850538095
John Lang <john.lang@tescobank.com>
2018-03-11 18:04:02 +0000

    shutdown

--- codedeploy.py
+++ codedeploy.py
@@ -102,7 +102,7 @@ lcf = template.add_resource(LaunchConfiguration(
 
 asg = template.add_resource(AutoScalingGroup(
     "asg",
-    DesiredCapacity="1",
+    DesiredCapacity="0",
     HealthCheckGracePeriod="10",
     HealthCheckType='EC2',
     LaunchConfigurationName=Ref(lcf),
